e4c73aac4eb1265cf9fb8e3f03376b55f5b02dfb
cono200 <tatattian04@gmail.com>
2023-10-16 09:13:34 -0700

    version 2

---
+++ CalculadoraIMCMVVB_SLE/Vistas/VcalcularIMC.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculadoraIMCMVVB_SLE.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VcalcularIMC : ContentPage
+    {
+        public VcalcularIMC()
+        {
+            InitializeComponent();
+        }
+    }
+}
